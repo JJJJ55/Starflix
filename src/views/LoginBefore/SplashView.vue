957ab0e6aa12f6c7bedb9ba68527caab5cc8e0b6
오진영 <wlsdud6221@naver.com>
2024-05-13 17:36:06 +0900

    스플래쉬 화면 구현

---
+++ src/views/LoginBefore/SplashView.vue
@@ -0,0 +1,26 @@
+<script setup></script>
+
+<template>
+  <div class="content">
+    <img
+      class="img-fluid logo"
+      src="@/assets/img/ssafyPJ_logo.png"
+      width="40%"
+      height="20%"
+      alt="logo"
+    />
+  </div>
+</template>
+
+<style scoped>
+.content {
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+.logo {
+  display: block;
+}
+</style>
