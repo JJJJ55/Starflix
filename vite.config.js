a8440873b6fe97274d0428b22760287696c6bd68
오진영 <wlsdud6221@naver.com>
2024-05-13 17:34:19 +0900

    ESLine 오류 해결

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,21 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import VueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import VueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
+  //esline 에러 화면에 표시안되고 터미널에만 표시
+  devServer: {
+    client: {
+      overlay: false,
+    },
+  },
+  plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
