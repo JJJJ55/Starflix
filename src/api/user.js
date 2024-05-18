907f06d1e082ac2830a4036ac5ec6ed085648101
JinY <wlsdud6221@naver.com>
2024-05-18 23:24:16 +0900

    user api 추가

--- src/api/user.js
+++ src/api/user.js
@@ -8,20 +8,62 @@ async function userLogin(param, success, fail) {
 }
 
 async function findById(userid, success, fail) {
-  // ac 토큰 유효성 검사
+  // 회원정보 (강의에서 회원인증)
   local.defaults.headers['Authorization'] =
     sessionStorage.getItem('accessToken');
   await local.get(`/user/${userid}`).then(success).catch(fail);
 }
 
 async function tokenRegeneration(user, success, fail) {
-  // ac토큰 재발급
+  // ac 재발급
   await local.post(`user/refresh`, user).then(success).catch(fail);
 }
 
-async function userlogout(userid, success, fail) {
+async function userlogout(id, success, fail) {
   // 로그아웃
-  await local.get(`/user/logout`, userid).then(success).catch(fail);
+  const params = {
+    userId: id,
+  };
+  // await local.get(`/user/logout?userId=${id}`).then(success).catch(fail);
+  await local.get(`/user/logout`, { params }).then(success).catch(fail);
 }
 
-export { userLogin, findById, tokenRegeneration, userlogout };
+async function userInsert(user, success, fail) {
+  //회원정보 수정
+  await local.post(`/user`, user).then(success).catch(fail);
+}
+
+async function userUpdate(userid, success, fail) {
+  //회원정보 수정
+  await local.put(`/user/${userid}`).then(success).catch(fail);
+}
+
+async function userDelete(userid, success, fail) {
+  // 회원정보 삭제
+  await local.delete(`/user/${userid}`).then(success).catch(fail);
+}
+
+async function jjimDelete(param, success, fail) {
+  //회원정보 수정
+  await local
+    .post(`/user/${param.userid}/unJjim`, param)
+    .then(success)
+    .catch(fail);
+}
+
+async function jjimList(userid, success, fail) {
+  //회원정보 수정
+  await local.get(`/user/${userid}/starPlaces`).then(success).catch(fail);
+}
+
+export {
+  userLogin,
+  findById,
+  tokenRegeneration,
+  userlogout,
+  userInsert,
+  userUpdate,
+  userDelete,
+  jjimDelete,
+  jjimList,
+};
