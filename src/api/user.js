585981074d3f4fea4b8e520ed0963b23a40c83e5
JinY <wlsdud6221@naver.com>
2024-05-20 02:07:52 +0900

    pinia 영속성 부여

--- src/api/user.js
+++ src/api/user.js
@@ -4,19 +4,19 @@ const local = localAxios();
 
 async function userLogin(param, success, fail) {
   // 로그인
-  await local.post(`/user/login`, param).then(success).catch(fail);
+  await local.post(`/users/login`, param).then(success).catch(fail);
 }
 
 async function findById(userid, success, fail) {
   // 회원정보 (강의에서 회원인증)
   local.defaults.headers['Authorization'] =
     sessionStorage.getItem('accessToken');
-  await local.get(`/user/${userid}`).then(success).catch(fail);
+  await local.get(`/users/${userid}`).then(success).catch(fail);
 }
 
 async function tokenRegeneration(user, success, fail) {
   // ac 재발급
-  await local.post(`user/refresh`, user).then(success).catch(fail);
+  await local.post(`users/refresh`, user).then(success).catch(fail);
 }
 
 async function userlogout(id, success, fail) {
@@ -25,35 +25,22 @@ async function userlogout(id, success, fail) {
     userId: id,
   };
   // await local.get(`/user/logout?userId=${id}`).then(success).catch(fail);
-  await local.get(`/user/logout`, { params }).then(success).catch(fail);
+  await local.get(`/users/logout`, { params }).then(success).catch(fail);
 }
 
 async function userInsert(user, success, fail) {
   //회원정보 수정
-  await local.post(`/user`, user).then(success).catch(fail);
+  await local.post(`/users`, user).then(success).catch(fail);
 }
 
-async function userUpdate(userid, success, fail) {
+async function userUpdate(user, success, fail) {
   //회원정보 수정
-  await local.put(`/user/${userid}`).then(success).catch(fail);
+  await local.put(`/users/${user.userId}`, user).then(success).catch(fail);
 }
 
 async function userDelete(userid, success, fail) {
   // 회원정보 삭제
-  await local.delete(`/user/${userid}`).then(success).catch(fail);
-}
-
-async function jjimDelete(param, success, fail) {
-  //회원정보 수정
-  await local
-    .post(`/user/${param.userid}/unJjim`, param)
-    .then(success)
-    .catch(fail);
-}
-
-async function jjimList(userid, success, fail) {
-  //회원정보 수정
-  await local.get(`/user/${userid}/starPlaces`).then(success).catch(fail);
+  await local.delete(`/users/${userid}`).then(success).catch(fail);
 }
 
 export {
@@ -64,6 +51,4 @@ export {
   userInsert,
   userUpdate,
   userDelete,
-  jjimDelete,
-  jjimList,
 };
